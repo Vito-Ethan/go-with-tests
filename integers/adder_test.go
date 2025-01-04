c16ba60ffcd3f0f5708b1e8e0c992f59222da8cb
Vito-Ethan <evito@unr.edu>
2025-01-03 19:48:01 -0800

    docs: add note about example functions

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 )
 
+// NOTE: Like test functions, example functions also reside inside _test.go files
+// and they will show up as clickable examples in documentation at pkgsite
+
 func ExampleAdd() {
 	sum := Add(1, 5)
 	fmt.Println(sum)
